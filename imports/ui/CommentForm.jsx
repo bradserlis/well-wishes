749e6bc1346f99b558f254160fc8c1912c95e6bc
bradserlis <bradserlis@yahoo.com>
2020-01-29 21:17:45 -0800

    Add CommentForm

--- imports/ui/CommentForm.jsx
+++ imports/ui/CommentForm.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Meteor } from 'meteor/meteor';
+import ReactDOM from 'react-dom';
 import { 
   Container, 
   Form,
@@ -10,9 +11,20 @@ import {
 } from 'semantic-ui-react';
 
 class PostForm extends Component {
+  constructor(props){
+    super(props)
+  }
+  
   handleSubmit = () => {
     event.preventDefault();
+      // Find the text field via the React ref
+    const content = ReactDOM.findDOMNode(this.refs.commentContentInput).value.trim();
+    const postId = this.props.post._id;
 
+    Meteor.call('comments.insert', content, postId);
+    
+    // Clear form
+    ReactDOM.findDOMNode(this.refs.commentContentInput).value = '';
   }
   
   render(){
