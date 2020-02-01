92433964ee0c2c61c7b3a001d64007dcd66d1c04
bradserlis <bradserlis@yahoo.com>
2020-01-31 17:50:20 -0800

    Add Post List and Active Post wip

--- imports/ui/Home.jsx
+++ imports/ui/Home.jsx
@@ -2,26 +2,46 @@ import React, { Component } from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
 import { Meteor } from 'meteor/meteor';
 import ReactDOM from 'react-dom';
-import { 
-  Container, 
+import {
+  Container,
   Form,
   Input,
-  Button, 
+  Button,
 } from 'semantic-ui-react'
 
 import { Posts } from '../api/posts';
-import Post from './Post.jsx';
+import Post from './Post';
 import PostForm from './PostForm';
 import MainLayout from '../client/layouts/MainLayout';
 
 class Home extends Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      activePostId: null
+    }
+  }
+
+  setActivePost = (e) => {
+    console.log('button value', e.target.value)
+    this.setState({
+      activePostId: e.target.value
+    })
+  }
+
   renderPosts = () => {
     return this.props.posts
-    .sort((a, b) => b.createdAt - a.createdAt)
-    .map((post) => (
-      <Post key={post._id} post={post} />
-    ));
+      .sort((a, b) => b.createdAt - a.createdAt)
+      .map((post) => (
+        <li key={post._id.toString()}>
+          <Button
+            value={post._id}
+            onClick={this.setActivePost}
+          >
+            {post.title}
+          </Button>
+        </li>
+      ));
   }
 
   handleSubmit = (event) => {
@@ -41,21 +61,27 @@ class Home extends Component {
     ReactDOM.findDOMNode(this.refs.contentInput).value = '';
     ReactDOM.findDOMNode(this.refs.titleInput).value = '';
   }
-  
-  render(){
+
+  render() {
     return (
       <Container>
-      <div className='home-container'>
-        <h1> Home page </h1>
-        <div id='home-form-container'>
-        <PostForm />
+        <div className='home-container'>
+          <h1> Home page </h1>
+          <div id='home-form-container'>
+            <PostForm />
+          </div>
+          <div className='home-posts'>
+            <div className='home-posts-list'>
+              <ul style={{ 'listStyle': 'none' }}>
+                {this.renderPosts()}
+              </ul>
+            </div>
+            <div className='home-post-active-post'>
+              {this.state.activePostId && <h3> {this.state.activePostId} </h3>}
+            </div>
+
+          </div>
         </div>
-        <div className='home-posts'>
-        <ul style={{'listStyle':'none'}}>
-          {this.renderPosts()}
-        </ul>
-        </div>
-      </div>
       </Container>
     )
   }
@@ -67,8 +93,9 @@ export default withTracker(() => {
   return {
     currentUser: Meteor.user(),
     posts: Posts
-    .find({owner: Meteor.userId()})
-    .fetch(),
+      .find({ owner: Meteor.userId() })
+      .fetch(),
   };
 })(Home);
 
+{/* <Post key={post._id} post={post} /> */ }
