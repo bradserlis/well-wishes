52a26b8cbb83007fbfd672f9ac3e1c28dbe3d953
bradserlis <bradserlis@yahoo.com>
2020-02-12 17:53:59 -0800

    code cleanup

--- imports/ui/Search.jsx
+++ imports/ui/Search.jsx
@@ -13,22 +13,10 @@ import Post from './Post.jsx';
 
 class Search extends Component {
 
-  renderPosts = () => {
-    return this.props.posts.map((post) => (
-      <Post key={post._id} post={post} />
-    ));
-  }
-
   renderOnePost = () => {
     let postChoice = Math.floor(Math.random() * this.props.posts.length);
-    console.log(this.props.posts[postChoice])
     return <Post key={this.props.posts[postChoice]._id} post={this.props.posts[postChoice]} />
   }
-
-  // componentDidMount = () => {
-  //   this.renderOnePost();
-  // }
-
 
   render() {
 
@@ -37,9 +25,11 @@ class Search extends Component {
         <header>
           <h1> Post List </h1>
         </header>
-        <ul>
-          {this.props.posts && this.renderOnePost()}
-        </ul>
+        <main>
+          <ul>
+            {this.props.posts && this.renderOnePost()}
+          </ul>
+        </main>
       </Container>
     );
   }
