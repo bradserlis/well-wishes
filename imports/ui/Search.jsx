f43d11bdee22fae0e1224f98cd4a8626d3aea05a
bradserlis <bradserlis@yahoo.com>
2020-02-12 17:32:30 -0800

    Add randomized post to Search screen

--- imports/ui/Search.jsx
+++ imports/ui/Search.jsx
@@ -1,31 +1,44 @@
 import React, { Component } from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
 import { Meteor } from 'meteor/meteor';
-import { 
-  Container, 
+import {
+  Container,
   Form,
   Input,
-  Button, 
+  Button,
 } from 'semantic-ui-react'
 
 import { Posts } from '../api/posts';
 import Post from './Post.jsx';
 
 class Search extends Component {
+
   renderPosts = () => {
     return this.props.posts.map((post) => (
       <Post key={post._id} post={post} />
     ));
-  } 
+  }
+
+  renderOnePost = () => {
+    let postChoice = Math.floor(Math.random() * this.props.posts.length);
+    console.log(this.props.posts[postChoice])
+    return <Post key={this.props.posts[postChoice]._id} post={this.props.posts[postChoice]} />
+  }
+
+  // componentDidMount = () => {
+  //   this.renderOnePost();
+  // }
+
 
   render() {
+
     return (
       <Container>
         <header>
           <h1> Post List </h1>
         </header>
         <ul>
-          {this.renderPosts()}
+          {this.props.posts && this.renderOnePost()}
         </ul>
       </Container>
     );
@@ -36,7 +49,7 @@ export default withTracker(() => {
   Meteor.subscribe('posts');
 
   return {
-    posts: Posts.find({ owner: { $ne: Meteor.userId()} }).fetch(),
+    posts: Posts.find({ owner: { $ne: Meteor.userId() } }).fetch(),
     currentUser: Meteor.user(),
   };
 })(Search);
