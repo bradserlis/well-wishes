8ef3b039ecd0a23ebd5c535afdfcd81ac1b04d3b
bradserlis <bradserlis@yahoo.com>
2020-03-06 18:05:25 -0800

    Add warning text when user cannot view comments on own posts

--- imports/ui/Post.jsx
+++ imports/ui/Post.jsx
@@ -117,7 +117,7 @@ export default class Post extends Component {
             (
               <Card.Content>
                 <ul style={{ 'listStyle': 'none', 'paddingInlineStart': '0' }}>
-                  {this.state.showComments && this.renderComments()}
+                  {this.state.showComments === true ? this.renderComments() : <p className='comment-timer-fail-message'> Please post to another user to see your comments! </p>}
                 </ul>
               </Card.Content>
             )
