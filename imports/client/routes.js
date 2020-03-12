87ae585a1916a6e085042443dc3dc771fac36369
bradserlis <bradserlis@yahoo.com>
2020-03-11 18:40:36 -0700

    Add auth check to routes, redirect on auth status

--- imports/client/routes.js
+++ imports/client/routes.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import {
   Switch,
-  Route
+  Route,
+  Redirect
 } from 'react-router-dom';
 import { render, ReactDOM } from 'react-dom';
+import { Meteor } from 'meteor/meteor';
+import { withTracker } from 'meteor/react-meteor-data';
 
 import Landing from '../ui/Landing';
 import Home from '../ui/Home';
@@ -11,17 +14,30 @@ import Search from '../ui/Search';
 import MainLayout from './layouts/MainLayout';
 
 class Router extends Component {
+
   render() {
-    return (
-      <Switch>
-        <Route exact path='/' render={props => <Landing {...props} />} />
-        <Route path='/home' render={props => <Home {...props} />} />
-        <Route path='/search' render={props => <Search {...props} />} />
-      </Switch>
-    )
+    if (this.props.currentUser) {
+      return (
+        <Switch>
+          <Route path='/home' render={props => <Home {...props} />} />
+          <Route path='/search' render={props => <Search {...props} />} />
+          <Redirect to='/home' />
+        </Switch>
+      )
+    }
+    else {
+      return (
+        <Switch>
+          <Route exact path='/' render={props => <Landing {...props} />} />
+          <Redirect to='/' />
+        </Switch>
+      )
+    }
   }
 }
 
-export default Router;
-
-  // document.getElementById('react-target');
+export default withTracker(() => {
+  return {
+    currentUser: Meteor.user()
+  };
+})(Router);
